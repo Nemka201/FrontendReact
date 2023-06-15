96861ee487035b0990a7090c81d9b81dc004019a
Nemka201 <nemka201@gmail.com>
2023-06-15 15:57:36 -0300

    fix

--- src/components/content/Slide.js
+++ src/components/content/Slide.js
@@ -2,29 +2,29 @@ import imagen1 from "../../assets/slide/1.jpg";
 
 function Slide() {
   return (
-    <div id="carousel" class="carousel slide">
+    <div id="carousel" className="carousel slide">
   <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src={imagen1} class="d-block w-100" alt="..."></img>
+      <img src={imagen1} className="d-block w-100" alt="..."></img>
       <div className="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div className="carousel-item">
-      <img src={imagen1} class="d-block w-100" alt="..."></img>
+      <img src={imagen1} className="d-block w-100" alt="..."></img>
       <div className="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div className="carousel-item">
-      <img src={imagen1} class="d-block w-100" alt="..."></img>
+      <img src={imagen1} className="d-block w-100" alt="..."></img>
       <div className="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
