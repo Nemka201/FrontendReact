5f7de526da78e10705155f3e6c4b66b05d687cd1
nanosp7 <spectormariano1@gmail.com>
2023-06-15 17:20:10 -0300

    Update CSS

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,15 @@
 function Navbar() {
   return (
-  <nav className="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
+  <nav className="navbar border-bottom border-bottom-dark">
     <div className="container">
-        <div className="col-xl-9 col-sm-8 col-lg-9"></div>
+        <div className="col-xl-9 col-sm-8 col-lg-9">
+          <h1 className="Title">Prive</h1>
+          </div>
         <div className="col-xl-1 col-sm-1 col-lg-1">
-          <button className="btn btn-outline-success me-2" id="login" type="button">Ingresar</button>
+          <button className="btn me-2" id="login" type="button">Ingresar</button>
         </div>
         <div className="col-xl-1 col-sm-1 col-lg-1">
-          <button className="btn btn-outline-danger me-2" id="register" type="button">Registrarse</button>
+          <button className="btn me-2" id="register" type="button">Registrarse</button>
         </div>
       </div>
   </nav>
