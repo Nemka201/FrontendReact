9b12b2f25f769a65cb9f4f6d07f8d31e0a6400fe
Nemka201 <nemka201@gmail.com>
2023-06-18 21:35:16 -0300

    Registerform

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
-  const [year, setAge] = useState('');
-
+  const [year, setYear] = useState('');
+  const [gender, setGender] = useState('');
+  const [tamaño, setTamaño] = useState('');
 
 
   const handleUsernameChange = (event) => {
@@ -17,8 +19,14 @@ function RegisterForm() {
   const handleNameChange = (event) => {
     setName(event.target.value);
   };  
-  const handleAgeChange = (event) => {
-    setAge(event.target.value);
+  const handleYearChange = (event) => {
+    setYear(event.target.value);
+  };
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+  };
+  const handleTamañoChange = (event) => {
+    setTamaño(event.target.value);
   };
 
   const handleSubmit = (event) => {
@@ -29,6 +37,30 @@ function RegisterForm() {
     // Restablecer los valores del formulario
     setUsername('');
     setPassword('');
+    setName('');
+    setYear('');
+    setGender('');
+    setTamaño('');
+
+    // Realiza una solicitud a la API utilizando Axios
+    axios.post('URL_DE_LA_API', {
+      username: username,
+      password: password,
+      name: name,
+      year: year,
+      gender:gender
+    })
+    .then(response => {
+    // Maneja la respuesta exitosa de la API
+      console.log(response.data);
+    })
+    .catch(error => {
+    // Maneja el error de la API
+      console.error(error);
+    });
+    
+    // Establecer un valor en la sesión
+      sessionStorage.setUser('clave', 'valor');
   };
 
   return (
@@ -61,6 +93,59 @@ function RegisterForm() {
           onChange={handleNameChange}
         />
       </div>
+      <div>
+        <label htmlFor="year">Fecha de Nacimiento:</label>
+        <input
+          type="date"
+          id="year"
+          value={year}
+          onChange={handleYearChange}
+        />
+      </div>
+      <div>
+        Genero: <br></br>
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          value="male"
+          checked={gender === 'male'}
+          onChange={handleGenderChange}
+        />
+        Hombre
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          value="female"
+          checked={gender === 'female'}
+          onChange={handleGenderChange}
+        />
+        Mujer
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          value="other"
+          checked={gender === 'other'}
+          onChange={handleGenderChange}
+        />
+        Otro
+      </label>
+      </div>
+      {(gender==='male')&&<div className="tamaño">
+      <div>
+        <label htmlFor="username">Longitud del pene:</label>
+        <input
+          type="number"
+          id="tamaño"
+          value={tamaño}
+          onChange={handleTamañoChange}
+        />
+      </div>
+      </div>}
       <button type="submit">Registrar</button>
     </form>
   );
