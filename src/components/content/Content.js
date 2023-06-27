07df4e4aa3a589c7e4be998e361522c6112ba465
nanosp7 <spectormariano1@gmail.com>
2023-06-27 02:20:46 -0300

    tontera

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -8,14 +8,13 @@ function Content() {
           <div className="col-4">
             <br></br><br></br><br></br>
             <section className="main-text-container">
-            <h1 className="main-text">Descubre un refugio íntimo en el corazón de Tucumán, donde la sensualidad y la privacidad se entrelazan en una experiencia inolvidable. Bienvenido a Prive, tu oasis de placer y discreción</h1>
+              <h1 className="main-text">Descubre un refugio íntimo en el corazón de Tucumán, donde la sensualidad y la privacidad se entrelazan en una experiencia inolvidable. Bienvenido a Prive, tu oasis de placer y discreción</h1>
             </section>
           </div>
           <div className="col-8">
             <section>
               <div className="contenedor-slide">
-                
-                <Slide/>
+                <Slide />
               </div>
             </section>
           </div>
